9f72ffcd50d1464a2b3b1fd7ede2a324fbbc37bc
Alex Budilovsky <alexbudy@gmail.com>
2015-12-31 11:52:19 -0800

    using basketball-reference.com instead

--- gen_scripts/scrape_player_data.py
+++ gen_scripts/scrape_player_data.py
@@ -1,14 +1,18 @@
-import urllib.request
+import urllib.request as ur
 from string import ascii_lowercase
+from bs4 import BeautifulSoup as BS
 
-base_url = 'http://www.databasebasketball.com/players/playerlist.htm?lt=' # append letter from 'a' to 'z'
+base_url = 'http://www.basketball-reference.com/players/' # append letter from 'a' to 'z'
 
 data = ''
 for c in ascii_lowercase:
-	if c == 'z':
+	if c == 'c':
 		url = base_url + c
-		page = urllib.request.urlopen(url)
-		data=page.read()
-		lines = data.splitlines()
-		for line in lines:
-			print(line)
+		with ur.urlopen(url) as url:
+			s = url.read()
+
+		soup = BS(s, "html.parser")
+
+		for row in soup('table', {'id' : 'players'})[0].tbody('tr'):
+			tds = row('td')
+			print(tds[0].string)
